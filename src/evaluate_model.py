b0697566c04fb85e04705b914c9e989d31df85a7
Jswig <andersjopo@gmail.com>
2020-07-19 20:23:42 -0700

    reformatred

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -9,49 +9,46 @@ import numpy as np
 from sklearn.model_selection import cross_validate
 from models import get_lr_model
 
-DATA_PATH = '../data/raw'
-FIG_PATH = '../paper/figures'
+DATA_PATH = "../data/raw"
+FIG_PATH = "../paper/figures"
 
 if __name__ == "__main__":
-    mpl.rcParams.update({
-        'figure.autolayout': True,
-        'figure.dpi': 150
-    })
+    mpl.rcParams.update({"figure.autolayout": True, "figure.dpi": 150})
     sns.set()
 
-    X_train = pd.read_csv(
-        os.path.join(DATA_PATH, 'training_set_features.csv')
-    ).drop('respondent_id',axis =1)
-    y_train = pd.read_csv(
-        os.path.join(DATA_PATH, 'training_set_labels.csv')
-    ).drop('respondent_id',axis =1)
+    X_train = pd.read_csv(os.path.join(DATA_PATH, "training_set_features.csv")).drop(
+        "respondent_id", axis=1
+    )
+    y_train = pd.read_csv(os.path.join(DATA_PATH, "training_set_labels.csv")).drop(
+        "respondent_id", axis=1
+    )
 
-    Cs = np.logspace(-2, 1, num = 10, base = 10)
+    Cs = np.logspace(-2, 1, num=10, base=10)
     means = []
     stds = []
     best_auc = 0
     for C in Cs:
         cv = cross_validate(
-            estimator = get_model(C),
-            X = X_train,
-            y = y_train,
-            cv = 5,
-            n_jobs = -1,
-            scoring = 'roc_auc',
+            estimator=get_model(C),
+            X=X_train,
+            y=y_train,
+            cv=5,
+            n_jobs=-1,
+            scoring="roc_auc",
         )
-        means.append(np.mean(cv['test_score']))
-        stds.append(np.std(cv['test_score']))
+        means.append(np.mean(cv["test_score"]))
+        stds.append(np.std(cv["test_score"]))
         if means[-1] > best_auc:
             best_C = C
-            best_auc = means[-1]    
+            best_auc = means[-1]
 
     fig, ax = plt.subplots()
     ax.plot(Cs, means)
-    ax.vlines(best_C, ymin = 0.82, ymax = 0.86,  colors = 'r', linestyle = 'dotted')
-    ax.annotate("$C = 0.464$ \n ROC AUC = 0.843", xy = (0.5, 0.835))
-    ax.set_xscale('log')
-    ax.set_xlabel('$C$')
-    ax.grid(axis = 'x')
-    ax.legend(['AUC', 'best $C$'])
-    ax.set_title('AUC for different values of $C$')
-    fig.savefig(os.path.join(FIG_PATH, 'lr_reg_performance.png')
+    ax.vlines(best_C, ymin=0.82, ymax=0.86, colors="r", linestyle="dotted")
+    ax.annotate("$C = 0.464$ \n ROC AUC = 0.843", xy=(0.5, 0.835))
+    ax.set_xscale("log")
+    ax.set_xlabel("$C$")
+    ax.grid(axis="x")
+    ax.legend(["AUC", "best $C$"])
+    ax.set_title("AUC for different values of $C$")
+    fig.savefig(os.path.join(FIG_PATH, "lr_reg_performance.png"))
--- src/models.py
+++ src/models.py
@@ -2,45 +2,44 @@ import sklearn
 from sklearn import impute, preprocessing, compose, pipeline, linear_model, multioutput
 from typing import List
 
+
 def _get_preprocessor(
-    num_features: List[str],
-    cat_features: List[str]
+    num_features: List[str], cat_features: List[str]
 ) -> pipeline.Pipeline:
 
-    num_transformer = pipeline.Pipeline([
-        ('scale', preprocessing.StandardScaler()),
-        ('impute', impute.KNNImputer(n_neighbors = 10))
-    ])
+    num_transformer = pipeline.Pipeline(
+        [
+            ("scale", preprocessing.StandardScaler()),
+            ("impute", impute.KNNImputer(n_neighbors=10)),
+        ]
+    )
 
-    cat_transformer = pipeline.Pipeline([
-        ('impute', impute.SimpleImputer(
-            strategy = 'constant', 
-            fill_value = 'missing'
-        )),
-        ('encode', preprocessing.OneHotEncoder(drop = 'first'))
-    ])
+    cat_transformer = pipeline.Pipeline(
+        [
+            ("impute", impute.SimpleImputer(strategy="constant", fill_value="missing")),
+            ("encode", preprocessing.OneHotEncoder(drop="first")),
+        ]
+    )
 
-    preprocessor = compose.ColumnTransformer([
-        ('num', num_transformer, num_features),
-        ('cat', cat_transformer, cat_features)
-    ])
+    preprocessor = compose.ColumnTransformer(
+        [("num", num_transformer, num_features), ("cat", cat_transformer, cat_features)]
+    )
     return preprocessor
 
+
 def get_lr_model(
-    num_features: List[str], 
-    cat_features: List[str], 
-    C: float = 1.0
-) -> sklearn.base.BaseEstimator: 
+    num_features: List[str], cat_features: List[str], C: float = 1.0
+) -> sklearn.base.BaseEstimator:
 
-    model = pipeline.Pipeline([
-        ('pre', _get_preprocessor(num_features, cat_features)),
-        ('model', multioutput.MultiOutputClassifier(
-            linear_model.LogisticRegression(
-                penalty = 'l1', 
-                C = C,
-                solver = 'saga'
-            )
-        ))
-    ])
+    model = pipeline.Pipeline(
+        [
+            ("pre", _get_preprocessor(num_features, cat_features)),
+            (
+                "model",
+                multioutput.MultiOutputClassifier(
+                    linear_model.LogisticRegression(penalty="l1", C=C, solver="saga")
+                ),
+            ),
+        ]
+    )
     return model
-
